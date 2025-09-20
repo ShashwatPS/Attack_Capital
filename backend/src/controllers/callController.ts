0793f2bafcbdc2030e7cb8cf63ea53c9ef9b6289
Shashwat Singh <shashwat123student@gmail.com>
2025-09-20 14:10:15 +0530

    Configured pagination in getCalls

--- backend/src/controllers/callController.ts
+++ backend/src/controllers/callController.ts
@@ -3,13 +3,21 @@ import type { Request, Response } from "express";
 export const getCalls = async (req: Request, res: Response) => {
     const { botID } = req.params;
 
+    const pageNo = Number(req.query.page) || 1;
+    const limit = 10;
+
+    const offset = (pageNo - 1) * limit;
+
     const apiKey = process.env.OPENMIC_API_KEY;
 
     if (!botID) {
         return res.status(400).json({ error: "Bot ID not found" });
     }
 
-    const url = `https://api.openmic.ai/v1/calls?bot_id=${botID}`;
+    const url = new URL(`https://api.openmic.ai/v1/calls`);
+    url.searchParams.append('bot_id', String(botID));
+    url.searchParams.append('limit', String(limit));
+    url.searchParams.append('offset', String(offset));
 
     const options = {
         method: "GET",
