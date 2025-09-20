497d8a9a568b2524e5bea31b678a9e2dd8a1a5f1
Shashwat Singh <shashwat123student@gmail.com>
2025-09-20 19:44:24 +0530

    health check endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import botRoutes from './routes/botRoutes.js';
 import callRoutes from './routes/callRoutes.js';
 import hookRoutes from './routes/hookRoutes.js'
+import type { Request, Response } from 'express';
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,10 @@ app.use(express.json());
 app.use('/api/bot', botRoutes);
 app.use('/api/call', callRoutes);
 app.use('/webhook', hookRoutes);
+
+app.get('/health', (req: Request, res: Response)=>{
+  res.status(200);
+})
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
